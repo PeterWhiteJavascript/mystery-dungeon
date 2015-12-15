5504d4c4c530f3a9b892ab1101735d8760341737
PeterWhiteJavascript <pedroblanco3257@gmail.com>
2015-12-14 18:15:23 -0800

    Initial

---
+++ interactable_data.js
@@ -0,0 +1,22 @@
+Quintus.InteractableData = function(Q){
+Q.intData={
+    first_plains2_0:{
+        NPC0:{
+            textNum:0,
+            //On text num 0, give 1 potion
+            items:[[0,{amount:1,item:"Potion"}]],
+            text:[
+                ["Hello, I'm test.","Here's a Potion!","By the way, this area will be expanded further in time.",{changeText:1}],
+                ["I've given away my potion, baka!"]
+            ]
+            
+            
+        },
+        NPC1:{
+            textNum:0,
+            items:[],
+            text:[["Hello, I'm test2.","By the way, this area will be expanded further in time."]]
+        }
+    },
+};
+};
