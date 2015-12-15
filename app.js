0e3715d847bcdb0027a38a306327f2e35e0b9cc3
PeterWhiteJavascript <pedroblanco3257@gmail.com>
2015-12-15 14:56:41 -0800

    mystery-dungeon

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-console.log("a");
+
 app.use(express.static(__dirname + '/public'));
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
 
 app.get('/', function(req, res){
   res.render('/index.html');
